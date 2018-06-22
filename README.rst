37f268d4350bfa70221bcba79ab830333efac5ac
sylhare <sylhareng@gmail.com>
2018-06-22 09:34:20 -0400

    Add package files

---
+++ README.rst
@@ -0,0 +1,8 @@
+Gitlab-stats
+============
+
+This work with python **3.6.5**
+
+In order to make is work:
+
+-  Create a ``GITLAB_TOKEN`` env variable with your access token.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='gitlab_stats',
+      version='1.0.0',
+      description='Get gitlab stats',
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='gitlab pipeline stats build',
+      url='',
+      author='sylhare',
+      author_email='sylhare@outlook.com',
+      license='MIT',
+      packages=find_packages(exclude=['docs']),
+      include_package_data=True,
+      install_requires=['requests'],
+      tests_require=['pytest'],
+      extras_require={
+          'test': ['coverage', 'pytest'],
+      },
+      entry_points={
+          'console_scripts': [
+              'gitlab_stats = gitlab_stats.cli:main'
+          ]
+      })
