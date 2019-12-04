23fbc53f30e09c122ebe8259ef6850756e454d7e
sylhare <sylhare@outlook.com>
2019-12-04 01:12:53 -0500

    #12 Update classifiers

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 with open('README.rst') as file:
     long_description = file.read()
 
-setup(name='gitlab-stats',
-      version='1.2.0',
+setup(name='gitlab_stats',
+      version='1.2.1',
       description='CLI to get pipeline stats from gitlab API v4 using gitlab project ID',
       long_description=long_description,
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.6',
       ],
